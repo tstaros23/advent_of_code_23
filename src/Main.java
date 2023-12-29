0ca651fcb7165d60ef6f2bcb17ec9e1d2b7dac4e
Ted Staros <tstaros23@gmail.com>
2023-12-29 14:56:36 -0700

    Add comments before refactoring hash

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,6 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         HashMap hash = fileReader("day14smallinput.txt");
-        if (hash == null) {
-            System.out.println('h');
-        }
-
-        // make a function that reads the file
-        // could try getting the indexOf for each # line by line to store in a hash for future use
-        // each row could have an array list of values in the hash
-        // somehow need to keep track of the columns that go with the rows
-        // column key row value to show where each # is located
-        // if there are no rows or column hash key is null, then move 0 to the top
-
-        // to do this maybe making an array from a range line by line to get the column keys and use a counter for each row
-        // scanning character by character might be best
     }
     public static HashMap fileReader(String file) {
         HashMap<Integer, ArrayList<Integer>> hash = new HashMap<>();
@@ -30,30 +17,32 @@ public class Main {
             int index = 0;
 
             while ((line = bufferedReader.readLine()) != null) {
+                // read file line by line using buffering
                 String array = line;
                 for (int j = 0; j < array.length(); j++) {
-                    // if we are adding key value for the first time,  we check if the key is there then put in new column and arraylist
+                    // if  adding key value for the first time,  check if the key is there then put in new row number and arraylist
                     if (!hash.containsKey(rowCount)) {
                         hash.put(rowCount, new ArrayList<>());
                     }
                     else {
-                        // get the index position of one # at a time and update j to that index position
+                        // if the index is 0 then get the first occurence of the #
                         if (index == 0) {
                             index = array.indexOf('#');
                         }
+                        // if no # are found then break the iteration
                         if (index == -1) {
                             break;
                         }
+                        // otherwise add the index to the arraylist associated with the row
                         else {
                             hash.get(rowCount).add(index);
+                            // get the next index starting from where the previous index position left off
+                            // by using a substring and starting place in the indexOf function
                             index = array.indexOf('#', index + 1);
                         }
-                        // should be indexOf of a substring
-
-                        // add value if it is not - 1
-                        // get the array list from the column count, and add the current char index to the array
                     }
                 }
+                // reassign index back to zero after each iteration of rows and increase row count
                 index = 0;
                 rowCount++;
             }
