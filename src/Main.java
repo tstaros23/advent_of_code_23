69b53d1ffbe20e20d627c5259acc8e69716226d5
Ted Staros <tstaros23@gmail.com>
2023-12-29 17:21:28 -0700

    Making a plan

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap hash = createIndexHash("day14smallerinput.txt");
+        HashMap cubedHash = cubedShapedRocksHash("day14smallerinput.txt");
+        HashMap roundHash = roundShapedRocksHash(cubedHash);
     }
     // refactor file reader hash so indexOf values are keys and the arraylists hold rows to make process easier
     // later on to make 0 hash of updated index positions
@@ -14,7 +15,7 @@ public class Main {
     // each time I get the indexOf values line by line, check if the indexOf value already exists in the hash keys or not
     // if not then add it to the hash and a new arrayList as the value
     // otherwise, do previous logic to get each indexOf and add the current row number to the arraylist
-    public static HashMap createIndexHash(String file) {
+    public static HashMap cubedShapedRocksHash(String file) {
         HashMap<Integer, ArrayList<Integer>> hash = new HashMap<>();
         try (FileReader fileReader = new FileReader(file);
              BufferedReader bufferedReader = new BufferedReader(fileReader)) {
@@ -56,8 +57,13 @@ public class Main {
         return hash;
     }
     // update index hash so that each 0 moves as North as possible stopping under # if applicable
-    public static void updateIndexHash(HashMap<Integer, ArrayList<String>> map) {
-        
+    public static HashMap roundShapedRocksHash(HashMap<Integer, ArrayList<String>> map) {
+        HashMap<Integer, ArrayList<Integer>> hash = new HashMap<>();
+        // using almost the same logic as above we can get the index positions of each 0, column
+        // use the index position to find the key value from the map hash being passed in the params
+        // find the highest value from the array list and subtract 1 to get the row for the 0 rock
+        // the map hash must also be updated so that row where the 0 is moved to, is added to the array list
+        return hash;
     }
     // third step is to take each row number from key value arraylist and multiply it by the arraylist length to get
     // the column total to total in the end for the answer
